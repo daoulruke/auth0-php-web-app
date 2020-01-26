ff80e919325aa020262c57bf4fb9383696dc3759
CHRISTOPHER <chris@sitevitals.net>
2020-01-26 23:16:48 +1300

    UPDATE

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 ?>
 <html>
     <head>
-        
+
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -43,7 +43,7 @@
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
         <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
 
-        <link href="public/app.css" rel="stylesheet">
+        <link href="public/css/app.css" rel="stylesheet">
 
 
 
